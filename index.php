cdac214679e272e2690036e8db24786f445c4e36
Theofilusarifin <theofilusarifin@gmail.com>
2022-05-27 08:46:22 +0700

    Update

--- index.php
+++ index.php
@@ -6,34 +6,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
+    <link rel="stylesheet" href="assets/index.css">
 </head>
 
 <body>
-<form action="login.php" method="post">
-        <h2>LOGIN</h2>
-        <?php if (isset($_GET['error'])) {
-            switch($_GET['error']) {
-                case 1:?>
-                    <p class="error"> <?php echo "Username and Password is REQUIRED."; ?> </p>
-            <?php break;
-                case 2:?>
-                    <p class="error"> <?php echo "Username is REQUIRED."; ?> </p>
-            <?php break;
-                case 3:?>
-                    <p class="error"> <?php echo "Password is REQUIRED."; ?> </p>
-            <?php break;
-                case 4:?>
-                    <p class="error"> <?php echo "Incorrect Username or Password is REQUIRED."; ?> </p>
-            <?php break;?>
-        <?php }} ?>
-        <label for="uname">Username: </label>
-        <input type="text" name="uname" placeholder="Username">
-        <br>
-        <label for="pass">Password: </label>
-        <input type="password" name="pass" placeholder="Password">
-        <br>
-        <button type="login" value="login">Login</button>
-    </form>
+    <div class="container">
+        <img src="assets/cover.png" alt="">
+        <form action="login.php" method="POST">
+            <div class="content">
+                <div class="rectangle">
+                    <p>LOGIN</p>
+                    <?php if (isset($_GET['error'])) {
+                        switch ($_GET['error']) {
+                            case 1: ?>
+                                <p class="error"> <?php echo "Username and Password is REQUIRED."; ?> </p>
+                            <?php break;
+                            case 2: ?>
+                                <p class="error"> <?php echo "Username is REQUIRED."; ?> </p>
+                            <?php break;
+                            case 3: ?>
+                                <p class="error"> <?php echo "Password is REQUIRED."; ?> </p>
+                            <?php break;
+                            case 4: ?>
+                                <p class="error"> <?php echo "Incorrect Username or Password is REQUIRED."; ?> </p>
+                                <?php break; ?>
+                    <?php }
+                    } ?>
+                    <label for="uname">Username: </label>
+                    <input type="text" name="uname" placeholder="Username">
+                    <br>
+                    <label for="pass">Password: </label>
+                    <input type="password" name="pass" placeholder="Password">
+                    <br>
+                    <input class="mybutton" type="submit" value="Login">
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
 
+
 </html>
+
+
+
+
+
+
+
